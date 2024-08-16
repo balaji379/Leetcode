842472e41203c4ca45f0eb1172eb6b9c02fcd857
balaji379 <131160089+balaji379@users.noreply.github.com>
2024-08-16 20:44:19 +0530

    Time: 43 ms (5.13%), Space: 45.1 MB (43.49%) - LeetHub

---
+++ 1001-n-repeated-element-in-size-2n-array/1001-n-repeated-element-in-size-2n-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+           HashMap<Integer,Integer> map = new HashMap<>();
+           for(int val : nums){
+            if(map.containsKey(val)) 
+                map.replace(val,map.get(val)+1);
+            else map.put(val,1);
+           }
+           int max = Integer.MIN_VALUE;
+           int output = 0;
+           for(Map.Entry<Integer,Integer> box :map.entrySet()){
+                        if(box.getValue()>=max){
+                            output =box.getKey();
+                            max = box.getValue();
+                        }
+           }
+           System.out.println("the map value is :"+map);
+           return output;
+    }
+}
