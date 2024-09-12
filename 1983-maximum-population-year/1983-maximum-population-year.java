3922c2088403411a3ba22528f38575fe7b6fab65
balaji379 <131160089+balaji379@users.noreply.github.com>
2024-09-12 18:31:40 +0530

    Time: 22 ms (5.23%), Space: 43.1 MB (5.38%) - LeetHub

---
+++ 1983-maximum-population-year/1983-maximum-population-year.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int maximumPopulation(int[][] logs) {
+        List<Integer> deadYears = new LinkedList<>();
+        int[] population = new int[logs.length];
+        int count = 0, index = 0;
+         Arrays.sort(logs, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a, int[] b) {
+                return Integer.compare(a[0], b[0]);
+            }
+        });
+        for (int[] log : logs) {
+            System.out.println(Arrays.toString(log));
+        }
+        for (int[] data : logs) {
+            if (deadYears.isEmpty()) {
+                deadYears.add(data[1]);
+                population[index] = 1;
+            } else {
+                while (!deadYears.isEmpty() && deadYears.getFirst() <= data[0]) {
+                    deadYears.removeFirst();
+                }
+                if (deadYears.isEmpty()) {
+                    population[index] = 1;
+                    deadYears.add(data[1]);
+                    deadYears.sort((a, b) -> a - b);
+
+                } else {
+                    population[index] = deadYears.size() + 1;
+                    deadYears.add(data[1]);
+                    deadYears.sort((a, b) -> a - b);
+                }
+            }
+            index += 1;
+        }
+        int max = -1, year = -1;
+        for (int i = 0; i < population.length; i++) {
+            if (population[i] > max) {
+                max = population[i];
+                year = logs[i][0];
+            }
+        }
+        System.out.println("the dead years is :" + deadYears);
+        System.out.println("the answer is :" + Arrays.toString(population));
+        System.err.println("the maximim population is :" + year);
+        return year;
+    }
+}
