03a8d3c920dee2e80002402d8b441a9f676336bd
balaji379 <131160089+balaji379@users.noreply.github.com>
2024-09-18 20:18:09 +0530

    Time: 1 ms (93.62%), Space: 46.1 MB (5.5%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,5 +1,26 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        
-    }
+      int[] left = new int[nums.length];
+      int[] right = new int[nums.length];
+      int first = 0, last = 0;
+      for(int i = 0,j = nums.length-1; i<nums.length && j>=0 ; i++,j--){
+        if(i==0 || j==nums.length-1){
+            left[i] = 0;
+            right[j] = 0;
+        }  else {
+              left[i] = first ;
+              right[j] = last;
+
+        }
+     first += nums[i];
+     last += nums[j];
+      }
+    //   System.out.println("the left array is :"+Arrays.toString(left));
+    //   System.out.println("the right array is :"+Arrays.toString(right));
+      for(int i = 0; i <nums.length ; i++){
+        if(left[i] == right[i]) 
+            return i;
+      }
+      return -1 ;
+      }
 }
