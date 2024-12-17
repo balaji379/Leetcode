e5ac5a7a4b272ae03d2eaf496b5fae71f98db72b
balaji379 <131160089+balaji379@users.noreply.github.com>
2024-12-17 21:20:02 +0530

    Time: 0 ms (100%), Space: 42.6 MB (7.58%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -1,54 +1,47 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        boolean leftflag = true ,rightflag = false ;
-        List<List<Integer>> output  =  new ArrayList<>();
-        LinkedList<TreeNode> nodes = new LinkedList<>();
-        nodes.add(root);
-        if(root==null ) return output;
-        while(!nodes.isEmpty()){
-            int itr = 0 , len =nodes.size()  ,rightind = len ;
-            List<Integer> list = new LinkedList<>();
-            while(itr<len && !nodes.isEmpty()){
-                if(leftflag){
-                    TreeNode left = nodes.peek().left,
-                    right = nodes.peek().right;
-                    list.addFirst(nodes.poll().val);
-                    if(right!=null)
-                           nodes.add(right);
-                    if(left!=null)
-                           nodes.add(left);
-                }
-                if(rightflag){
-                       TreeNode left = nodes.peek().left,
-                    right = nodes.peek().right;
-                    list.add(nodes.poll().val);
-                    if(right!=null)
-                           nodes.add(right);
-                    if(left!=null)
-                           nodes.add(left);
-                }
-                itr+=1;
-            }
-            if(leftflag) { leftflag=false; rightflag=true ; }
-            else{ rightflag = false; leftflag=true; }
-            output.add(list);
+        List<List<Integer>> list = new ArrayList<>();
+        if (root == null) {
+            return list;
         }
-
-      return output;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        boolean evenOrder = false;
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int levelsize = queue.size();
+            for (int i = 0; i < levelsize; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+                if (evenOrder) {
+                    level.addFirst(node.val);
+                } else {
+                    level.add(node.val);
+                }
+            }
+            evenOrder = !evenOrder;
+            list.add(level);
+        }
+        return list;
     }
 }
