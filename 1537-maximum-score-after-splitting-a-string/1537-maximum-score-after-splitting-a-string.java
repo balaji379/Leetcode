023e6a21b09f8d1071a7ac2fd630cb134fd1e6e3
balaji379 <131160089+balaji379@users.noreply.github.com>
2025-01-02 20:22:31 +0530

    Time: 2 ms (39.38%), Space: 41.5 MB (64.46%) - LeetHub

---
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int maxScore(String s) {
+        int[] zeroPrefix = new int[s.length()];
+        int[] onePrefix = new int[s.length()];
+        int zero = 0, one = 0;
+        for (int i = 0; i < s.length(); i++){
+            if (s.charAt(i) == '0'){
+                zero += 1;
+                zeroPrefix[i] = zero;
+                onePrefix[i] = one;
+            }
+             if (s.charAt(i) == '1'){
+                one += 1;
+                onePrefix[i] = one;
+                zeroPrefix[i] = zero;
+            }
+        }
+         int max = -1;
+         if (zero == 0){
+            if (s.length() == 2)
+                return 1;
+            int sum = s.length() / 2;
+            if ((s.length() - sum) - sum == 0) {
+                return sum + 1;
+            }
+            else return sum + 2;
+        }
+         if (one == 0){
+             if (s.length() == 2)
+                return 1;
+             int sum = s.length() / 2;
+             if ((s.length() - sum) - sum == 0) {
+                return sum + 1;
+            }
+           else return sum + 2;
+        }
+        for (int i = 0; i < s.length() - 1; i++){
+            int sum = zeroPrefix[i] + (one - onePrefix[i]);
+            max = Math.max(sum,max);
+        }
+    
+        return max;
+    }
+}
