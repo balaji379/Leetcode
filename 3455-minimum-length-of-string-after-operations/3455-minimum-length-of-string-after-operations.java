3059e7d60ca93e5ccffc93b93d1a2e2befcd50ea
balaji379 <131160089+balaji379@users.noreply.github.com>
2025-01-13 09:51:21 +0530

    Time: 91 ms (17.88%), Space: 51 MB (7.95%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
@@ -6,10 +6,17 @@ class Solution {
         Map<Character, Integer> map = new HashMap<>();
         for (char c : str.toCharArray())
             map.put(c, map.getOrDefault(c, 0) + 1);
-        return map.values().stream().map(a -> {
-            if (a % 2 == 0)
-                return 2;
-            else return 1;
-        }).reduce((a, b) -> a + b).get();
+        int minlen = 0;
+        for(int count : map.values())
+           if(count % 2 == 0)
+               minlen += 2;
+            else 
+                minlen += 1;
+        return minlen;
+        // return map.values().stream().map(a -> {
+        //     if (a % 2 == 0)
+        //         return 2;
+        //     else return 1;
+        // }).reduce((a, b) -> a + b).get();
     }
 }
