cc44ac1dca959e21163512e21476639ac25efcf4
balaji379 <131160089+balaji379@users.noreply.github.com>
2025-01-14 14:35:37 +0530

    Time: 13 ms (22.66%), Space: 45.5 MB (76.3%) - LeetHub

--- 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.java
+++ 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.java
@@ -1,35 +1,29 @@
 class Solution {
     public int[] findThePrefixCommonArray(int[] A, int[] B) {
-        return findCarr(A,B);
+        return getCarrUsingStream(A,B);
     }
-    public  int[] findCarr(int[] nums1, int[] nums2) {
+ public  int[] getCarrUsingStream(int[] a, int[] b) {
         Map<Integer, Integer> map1 = new HashMap<>();
         Map<Integer, Integer> map2 = new HashMap<>();
-        int[] answer = new int[nums1.length];
-        int count = 0;
-
-        for (int i = 0; i < nums2.length; i++) {
-            map1.put(nums1[i], 1);
-            map2.put(nums2[i], 1);
-
-            if (map1.containsKey(nums2[i]) && map2.containsKey(nums1[i])) {
-                if (nums1[i] == nums2[i]) {
-                    count += 1;
-                    answer[i] = count;
-                } else {
-                    count += 2;
-                    answer[i] = count;
-                }
-            } else if (map1.containsKey(nums2[i])) {
-                count += 1;
-                answer[i] = count;
-            } else if (map2.containsKey(nums1[i])) {
-                count += 1;
-                answer[i] = count;
-            }
-            else
-              answer[i] = count;
-        }
-        return answer;
+        int[] count = {0};
+       return  IntStream.range(0,a.length)
+                .map(index ->{
+                    map1.put(a[index],1);
+                    map2.put(b[index],1);
+                    if (map1.containsKey(b[index]) && map2.containsKey(a[index])){
+                        if (a[index] == b[index])
+                            count[0] += 1;
+                        else
+                            count[0] += 2;
+                        return count[0];
+                    } else if (map1.containsKey(b[index])) {
+                        count[0] += 1;
+                        return count[0];
+                    } else if (map2.containsKey(a[index])) {
+                        count[0] += 1;
+                        return count[0];
+                    }
+                    return count[0];
+                }).toArray();
     }
 }
