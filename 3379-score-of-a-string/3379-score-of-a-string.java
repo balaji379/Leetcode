4e2d251db6cf3fbadc08b0c2d045fd41fcd33315
balaji379 <131160089+balaji379@users.noreply.github.com>
2025-01-22 22:57:25 +0530

    Time: 4 ms (3.13%), Space: 42.1 MB (60.92%) - LeetHub

--- 3379-score-of-a-string/3379-score-of-a-string.java
+++ 3379-score-of-a-string/3379-score-of-a-string.java
@@ -2,9 +2,11 @@ class Solution {
     public int scoreOfString(String s) {
         int left = 0 ,right = 1;
         int score = 0;
+        int len = s.length();
+        StringBuilder sb = new StringBuilder(s);
         System.out.println((int)s.charAt(0));
-        while (right < s.length()){
-           score += Math.abs(s.charAt(left++) - s.charAt(right++));
+        while (right < len){
+           score += Math.abs(sb.charAt(left++) - sb.charAt(right++));
         }
         return score;
     }
