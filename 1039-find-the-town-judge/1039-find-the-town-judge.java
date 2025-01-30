de72e4570a94be39a424d7b8c74e2666fa6e9d29
balaji379 <131160089+balaji379@users.noreply.github.com>
2025-01-30 21:10:37 +0530

    Time: 22 ms (9.13%), Space: 51.7 MB (94.61%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.java
+++ 1039-find-the-town-judge/1039-find-the-town-judge.java
@@ -1,23 +1,22 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        Set<Integer> set = new HashSet<>();
-        if (n == 1 ) 
-          return 1;
-        if( trust.length == 0 )
+        if (trust.length == 0 && n < 1)
            return -1;
-        int judge = trust[0][1];
-        boolean flag = true;
-        for(int i = 1; i <= trust.length; i++){
-            set.add(trust[i-1][0]);
+        if (trust.length == 0 && n > 1)
+           return -1;
+        if (n == 1)
+           return 1;
+        Map<Integer,Integer>  ajudge = new HashMap<>();
+        Map<Integer,Integer>  notajudge = new HashMap<>();
+        for (int[] judge : trust){
+           ajudge.put(judge[1],0);
+           notajudge.put(judge[0],0);
         }
-        System.out.print(set);
-        int count = 0,j = 0;
-       for (int i = 1; i<= n; i++){
-        if(!set.contains(i))
-            count += 1;
-            j = i;
-       }
-    
-        return (count == 1 ? (j == judge ? j : -1) : -1) ;
+        for (int[] judge : trust){
+            ajudge.put(judge[1] ,ajudge.get(judge[1])+1);
+            if (ajudge.get(judge[1]) == n -1 && !notajudge.containsKey(judge[1]))
+               return judge[1];
+        }
+        return -1;
     }
 }
