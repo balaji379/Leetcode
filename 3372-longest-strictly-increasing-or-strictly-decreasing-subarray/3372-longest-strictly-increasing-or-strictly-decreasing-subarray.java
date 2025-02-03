452f314c87d2893ef9bd13afc85fc3a855ca087e
balaji379 <131160089+balaji379@users.noreply.github.com>
2025-02-03 10:06:33 +0530

    Time: 8 ms (3.03%), Space: 44.3 MB (6.06%) - LeetHub

---
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int longestMonotonicSubarray(int[] nums) {
+         int max = Math.max(findMaxSub(true,nums),findMaxSub(false,nums));
+         System.out.print(max);
+         return max;
+    }
+    public int findMaxSub(boolean flag,int...nums){
+              Stack<Integer> stack = new Stack<>();
+              int max = 0;
+              for (int i = 0; i < nums.length; i++){
+                if (stack.isEmpty())
+                       stack.push(nums[i]);
+                if (flag){
+                    if (stack.peek() < nums[i])
+                        stack.push(nums[i]);
+                    else {
+                        max = Math.max(max,stack.size());
+                        stack = new Stack<Integer>();
+                        stack.push(nums[i]);
+                    }
+                }
+                else {
+                      if (stack.peek() > nums[i])
+                        stack.push(nums[i]);
+                    else {
+                        max = Math.max(max,stack.size());
+                        stack = new Stack<Integer>();
+                        stack.push(nums[i]);
+                    }
+                }
+              }
+              max = Math.max(max,stack.size());
+              return max;
+    }
+}
