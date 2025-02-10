2014ac88b8dbec0517298a81f05bd1906530a2ed
balaji379 <131160089+balaji379@users.noreply.github.com>
2025-02-10 22:56:20 +0530

    Time: 2 ms (25.44%), Space: 43.9 MB (88.18%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int compress(char[] chars) {
+        StringBuilder sb = new StringBuilder();
+        int len = chars.length;
+        for (int i = 0; i < len; i++){
+            int start = i;
+            int size = 0;
+            sb.append(chars[i]);
+            while (start < chars.length){
+                 if (chars[i] == chars[start])
+                     size += 1;
+                 else {
+                    break;
+                 }
+                 start += 1;
+            }
+            if (size > 1)
+              sb.append(size);
+            i = start - 1;
+        }
+        len = sb.length();
+        for (int i = 0; i < len; i++){
+            chars[i] = sb.charAt(i);
+        }
+        return sb.length();
+    }
+}
