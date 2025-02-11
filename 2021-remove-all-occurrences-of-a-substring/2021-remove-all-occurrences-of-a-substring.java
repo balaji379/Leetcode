3db8d3a75c7232aed2822f38775cf9cae6d47768
balaji379 <131160089+balaji379@users.noreply.github.com>
2025-02-12 00:10:20 +0530

    Time: 7 ms (43.37%), Space: 44.8 MB (19.65%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
@@ -1,8 +1,7 @@
 class Solution {
     public String removeOccurrences(String s, String part) {
-        while(s.contains(part)){
-            s = s.replaceFirst(part,"");
-        }
-        return s;
+       while (s.contains(part))
+         s = s.replaceFirst(part,"");
+       return s;
     }
 }
