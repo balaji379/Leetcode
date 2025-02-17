22692807853e8f701b13c8331fd09e526f575f6a
balaji379 <131160089+balaji379@users.noreply.github.com>
2025-02-17 16:18:10 +0530

    Time: 53 ms (9.4%), Space: 48 MB (5.2%) - LeetHub

--- 1160-letter-tile-possibilities/1160-letter-tile-possibilities.java
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.java
@@ -1,18 +1,16 @@
 class Solution {
     public int numTilePossibilities(String tiles) {
-     Set<String> dp = new HashSet<>();
         Set<String> checker = new HashSet<>();
-        return findPossibities("",tiles,new ArrayList<>(),checker,0,dp);
+        return findPossibities("",tiles,new ArrayList<>(),checker,0);
 //        System.out.println(checker);
     }
-    public static int findPossibities(String p,String up, List<String> list, Set<String> checker,int count,Set<String> dp){
-
-        if (dp.contains(p))
-             return count;
+    public static int findPossibities(String p,String up, List<String> list, Set<String> checker,int count){
+        
+        if(checker.contains(p))
+            return count;
 
         if (!p.isEmpty() && !checker.contains(p)){
             count += 1;
-            dp.add(p);
             checker.add(p);
         }
 
@@ -29,7 +27,7 @@ class Solution {
             StringBuilder temp = new StringBuilder(p);
             for (int j = 0; j < len + 1; j++){
                    temp.insert(j,c);
-                   count = findPossibities(temp.toString(),sb.toString(),list,checker,count,dp);
+                   count = findPossibities(temp.toString(),sb.toString(),list,checker,count);
                    temp.deleteCharAt(j);
             }
             sb.insert(i,c);
