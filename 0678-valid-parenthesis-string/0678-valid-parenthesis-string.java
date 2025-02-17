dcac7195eae5ed577cde935bf740a0a71d0081d0
balaji379 <131160089+balaji379@users.noreply.github.com>
2025-02-17 22:25:37 +0530

    Time: 0 ms (100%), Space: 41.4 MB (59.89%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -9,23 +9,23 @@ class Solution {
             } else if (c == ')') {
                 low--;
                 high--;
-            } else { // c == '*'
-                low--; // Treat '*' as ')'
-                high++; // Treat '*' as '('
+            } else {
+                low--; 
+                high++; 
             }
             
-            // low should not go below 0
+           
             if (low < 0) {
                 low = 0;
             }
             
-            // If high is negative, we have more ')' than we can match
+          
             if (high < 0) {
                 return false;
             }
         }
         
-        // At the end, low should be 0 for a valid string
+       
         return low == 0;
     }
 }
