20791f6e6c1780fc92df48557ab4aba9e5bccfd3
balaji379 <131160089+balaji379@users.noreply.github.com>
2025-02-21 20:55:54 +0530

    Time: 0 ms (100%), Space: 41.8 MB (54.83%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.java
+++ 0289-game-of-life/0289-game-of-life.java
@@ -1,9 +1,13 @@
 class Solution {
     public void gameOfLife(int[][] board) {
         int[][] ans = new int[board.length][board[0].length];
-        IntStream.range(0, board.length).forEach(i -> {
-            IntStream.range(0, board[i].length).forEach(j -> ans[i][j] = board[i][j]);
-        });
+        // IntStream.range(0, board.length).forEach(i -> {
+        //     IntStream.range(0, board[i].length).forEach(j -> ans[i][j] = board[i][j]);
+        // });
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++)
+                ans[i][j] = board[i][j];
+        }
         gameOfLifes(board, ans);
     }
 
