685c45e0beb28b4add1c49c2d721068a06e75a53
balaji379 <131160089+balaji379@users.noreply.github.com>
2025-02-21 07:29:50 +0530

    Time: 20 ms (98.92%), Space: 48.2 MB (10.24%) - LeetHub

--- 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.java
+++ 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.java
@@ -15,8 +15,11 @@
  */
 class FindElements {
     private TreeNode root;
+    private Set<Integer> container;
     public FindElements(TreeNode root) {
         this.root = root;
+        container = new HashSet<>();
+        container.add(0);
         if (this.root != null){
             root.val = 0;
             solve(this.root,false,false,0);
@@ -25,7 +28,7 @@ class FindElements {
     }
     
     public boolean find(int target) {
-          return findTarget(root , target);
+          return container.contains(target);
     }
 
     public void solve(TreeNode node,boolean left,boolean right,int headval){
@@ -33,25 +36,27 @@ class FindElements {
             return ;
         if(left){
                node.val = (2 * headval) + 1;
+               container.add(node.val);
         }
         if (right){
               
                node.val = (2 * headval) + 2;
+               container.add(node.val);
         }
         solve(node.left,true,false,node.val);
         solve(node.right,false,true,node.val);
     }
 
-    public boolean findTarget(TreeNode node,int target){
-        if (node == null)
-            return false;
+    // public boolean findTarget(TreeNode node,int target){
+    //     if (node == null)
+    //         return false;
         
-        if(node.val == target)
-             return true;
+    //     if(node.val == target)
+    //          return true;
 
-         return  (findTarget(node.right,target) || findTarget(node.left,target));  
+    //      return  (findTarget(node.right,target) || findTarget(node.left,target));  
         
-    }
+    // }
 }
 
 /**
