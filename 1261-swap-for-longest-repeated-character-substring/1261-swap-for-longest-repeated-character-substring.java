8886a85749cf43d2351d28d07e7a959474997c59
balaji379 <131160089+balaji379@users.noreply.github.com>
2025-04-22 21:11:52 +0530

    Time: 10 ms (39.29%), Space: 42.8 MB (33.33%) - LeetHub

--- 1261-swap-for-longest-repeated-character-substring/1261-swap-for-longest-repeated-character-substring.java
+++ 1261-swap-for-longest-repeated-character-substring/1261-swap-for-longest-repeated-character-substring.java
@@ -1,5 +1,48 @@
 class Solution {
     public int maxRepOpt1(String text) {
-        
+        char[] letters = text.toCharArray();
+        return findLongestSubString(letters);
     }
+
+    private static int findLongestSubString(char[] letters) {
+        int left = 0, max = 0, right = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : letters) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        char pre = letters[0];
+        int i = 0;
+        while (i < letters.length) {
+            while (i < letters.length && pre == letters[i]) {
+                left += 1;
+                i += 1;
+            }
+            int temp = i;
+            if (left != 0 && temp < letters.length - 1 && pre == letters[temp + 1]) {
+                temp += 1;
+                while (temp < letters.length && pre == letters[temp]) {
+                    right += 1;
+                    temp += 1;
+                }
+            } else {
+                if (left < map.get(pre))
+                    left += 1;
+                max = Math.max(max, left);
+            }
+            if ((left + right) == map.get(pre)) {
+                max = Math.max(max, (left + right));
+            } else if (right != 0 && (left + right) < map.get(pre)) {
+                max = Math.max(max, (left + right) + 1);
+            } else
+                max = Math.max(left, max);
+
+            if (i < letters.length) {
+                pre = letters[i];
+                left = 0;
+                right = 0;
+            }
+        }
+        return max;
+    }
+
 }
