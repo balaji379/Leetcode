c9537f3e922b2e9f314aa15d4e112e7ddeffdd9a
balaji379 <131160089+balaji379@users.noreply.github.com>
2025-05-07 23:36:20 +0530

    Time: 5 ms (20.61%), Space: 42.8 MB (10.27%) - LeetHub

---
+++ 1944-truncate-sentence/1944-truncate-sentence.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+      String[] words = s.split(" ");
+      String ans = "";
+      for (int i = 0; i < k; ++i)
+         ans += (" "+words[i]);
+      return ans.trim();
+    }
+}
