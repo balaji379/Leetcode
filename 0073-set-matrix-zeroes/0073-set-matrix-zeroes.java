6c4cbc324f94500ec6a7630e6f4127950896289f
balaji379 <131160089+balaji379@users.noreply.github.com>
2025-05-22 00:15:13 +0530

    Time: 1 ms (72.83%), Space: 45.6 MB (82.97%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,38 +1,32 @@
 class Solution {
-      public static void set0row(int[][] mat, int row) {
-        for (int col = 0; col < mat[row].length; col++) {
-            mat[row][col] = 0;
-        }
-    }
-
-    public static void set0col(int[][] mat, int col) {
-        for (int row = 0; row < mat.length; row++) {
-            mat[row][col] = 0;
-        }
-    }
-    public void setZeroes(int[][] mat) {
-        int[] row = new int[mat.length];
-        var max = -1;
-        for (int[] arr : mat)
-            max = Math.max(max, arr.length);
-        int[] col = new int[max];
-        int r = 0, c = 0;
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat[i].length; j++) {
-                if (mat[i][j] == 0) {
-                    row[i] = 1;
-                    col[j] = 1;
+    public void setZeroes(int[][] matrix) {
+            boolean[] row = new boolean[matrix.length];
+            boolean[] col = new boolean[matrix[0].length];
+            for (int i = 0; i < matrix.length; i++){
+                for (int j = 0; j < matrix[i].length; j++){
+                    if (matrix[i][j] == 0){
+                        row[i] = true;
+                        col[j] = true;
+                    }
                 }
             }
-        }
-        for (int i = 0; i < row.length; i++) {
-            if (row[i] == 1) {
-                set0row(mat, i);
+            for (int i = 0; i < row.length; i++){
+                if (row[i])
+                   setRowZero(matrix,i);
             }
+            for (int i = 0; i < col.length; i++){
+                if (col[i])
+                  setColZero(matrix,i);
+            }
+    }
+    public void setRowZero(int[][] mat,int row){
+        for (int i = 0; i < mat[row].length; ++i){
+            mat[row][i] = 0;
         }
-        for (int i = 0; i < col.length; i++) {
-            if (col[i] == 1)
-                set0col(mat, i);
+    }
+    public void setColZero(int[][] mat,int col){
+        for (int i = 0; i < mat.length; ++i){
+            mat[i][col] = 0;
         }
     }
 }
