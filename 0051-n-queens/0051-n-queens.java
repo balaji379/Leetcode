5de422b5f9dd31c000a72139af98a9281b15faf4
balaji379 <131160089+balaji379@users.noreply.github.com>
2025-07-02 22:01:40 +0530

    Time: 2 ms (86.09%), Space: 44.8 MB (87.96%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,53 +1,53 @@
 class Solution {
-    List<List<String>> outer = new ArrayList<>();
+    List<List<String>> out = new ArrayList<>();
     public List<List<String>> solveNQueens(int n) {
-         solve(new char[n][n],0,n);
-         return outer;
+         solve(new char[n][n],n,0);
+         return out;
     }
-     public  void solve(char[][] mat,int col,int n){
-        if (n == 0){
+    public void solve(char[][] mat, int n, int col) {
+        if (n == 0) {
             List<String> list = new ArrayList<>();
-            for (int i = 0; i < mat.length; ++i){
+            for (int i = 0; i < mat.length; i++){
                 StringBuilder sb = new StringBuilder();
-                for (int j = 0; j < mat[i].length; ++j){
+                for (int j = 0; j < mat[i].length; j++){
                     if (mat[i][j] == 'Q')
-                         sb.append('Q');
+                          sb.append('Q');
                     else sb.append('.');
                 }
                 list.add(sb.toString());
             }
-            outer.add(list);
-            return;
-        }
-        for (int i = 0; i < mat.length; i++){
-            if (isSafe(mat,i,col)){
-                mat[i][col] = 'Q';
-                solve(mat,col + 1,n - 1);
-                mat[i][col] = ' ';
+            this.out.add(list);
+        } else {
+            for (int i = 0; i < mat.length; i++) {
+                if (isSafe(mat, i, col)) {
+                    mat[i][col] = 'Q';
+                    solve(mat, n - 1, col + 1);
+                    mat[i][col] = ' ';
+                }
             }
         }
     }
 
-    private  boolean isSafe(char[][] mat, int row, int col) {
-        int temprow = row,tempcol = col;
-//        left
-        for(int i = col; i >=0; --i){
+    private boolean isSafe(char[][] mat, int row, int col) {
+        int i = col;
+        while (i >= 0) {
             if (mat[row][i] == 'Q')
-                  return false;
+                return false;
+            i -= 1;
         }
-//        top digonal
-        while (row >= 0 && col >= 0){
+        i = row;
+        int j = col;
+        while (row >= 0 && col >= 0) {
             if (mat[row][col] == 'Q')
-                 return false;
+                return false;
             row -= 1;
             col -= 1;
         }
-//        bottom digonal
-        row = temprow;
-        col = tempcol;
-        while (row < mat.length && col >=0){
+        row = i;
+        col = j;
+        while (row < mat.length && col >= 0) {
             if (mat[row][col] == 'Q')
-                 return false;
+                return false;
             row += 1;
             col -= 1;
         }
