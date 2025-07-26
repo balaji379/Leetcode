b840e290b3b4d8614757636913e84d9abab0c659
balaji379 <131160089+balaji379@users.noreply.github.com>
2025-07-26 10:28:34 +0530

    Time: 2 ms (99.27%), Space: 54.6 MB (7.51%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.java
+++ 1039-find-the-town-judge/1039-find-the-town-judge.java
@@ -1,21 +1,15 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        if (trust.length == 0 && n < 1)
-           return -1;
-        if (trust.length == 0 && n > 1)
-           return -1;
-        if (n == 1)
-           return 1;
-        Map<Integer,Integer>  ajudge = new HashMap<>();
-        Map<Integer,Integer>  notajudge = new HashMap<>();
-        for (int[] judge : trust){
-           ajudge.put(judge[1],0);
-           notajudge.put(judge[0],0);
+        if(n == 1) return 1;
+        int[] num = new int[n + 1];
+        for (int i = 0; i < trust.length; i++) {
+            num[trust[i][0]] -= 1;
+            num[trust[i][1]] += 1;
         }
-        for (int[] judge : trust){
-            ajudge.put(judge[1] ,ajudge.get(judge[1])+1);
-            if (ajudge.get(judge[1]) == n -1 && !notajudge.containsKey(judge[1]))
-               return judge[1];
+        for (int i = 0; i < num.length; i++) {
+            if(num[i] == n - 1){
+                return i;
+            }
         }
         return -1;
     }
