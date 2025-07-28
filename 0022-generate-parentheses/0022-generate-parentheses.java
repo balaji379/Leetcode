4fd3dc8b88de2fbaa14ee13a25575b7d5ab18aea
balaji379 <131160089+balaji379@users.noreply.github.com>
2025-07-28 15:47:16 +0530

    Time: 68 ms (5.3%), Space: 45.6 MB (5.11%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,62 +1,20 @@
 class Solution {
+    HashSet<String> set = new HashSet<>();
     public List<String> generateParenthesis(int n) {
-        List<String> answer = new ArrayList<>();
-        Set<String> set = new HashSet<>();
-        generateParentheses(n, n - 1, n, "(", set, answer);
-        return answer;
+        solve(new StringBuilder("()"),1,n);
+        ArrayList<String> ans = new ArrayList<>();
+        for(String i: set) ans.add(i);
+        return ans;
     }
-
-     public static void generateParentheses(int n, int open, int close, String val, Set<String> set,
-            List<String> answer) {
-        if (open == 0 && close == 0) {
-            if (!set.contains(val)) {
-                answer.add(val);
-                set.add(val);
-                return;
-            }
-        }
-        if (open == 1 && close == 1) {
-            val += "()";
-            if (!set.contains(val)) {
-                answer.add(val);
-                set.add(val);
-            }
+    public void solve(StringBuilder sb,int c,int n){
+        if(c == n){
+            set.add(sb.toString());
             return;
         }
-        List<String> combination = new ArrayList<>();
-        String tempstr = "";
-        for (int i = 0; i < open; i++) {
-            tempstr += "(";
-            combination.add(tempstr);
-        }
-        tempstr = "";
-        if (open != 0) {
-            for (int i = 0; i < (close - open); i++) {
-                tempstr += ")";
-                combination.add(tempstr);
-            }
-        } else {
-            tempstr = ")";
-            tempstr = tempstr.repeat(close);
-            combination.add(tempstr);
-            // close = 0;
-        }
-        for (String str : combination) {
 
-            if (open > 0 && str.charAt(0) == '(') {
-                int newopen = Math.abs(open - str.length());
-                String tempval = val + str;
-                generateParentheses(n, newopen, close, tempval, set, answer);
-            }
-
-            else if (close > 0 && str.charAt(0) == ')') {
-
-                int newclose = Math.abs(close - str.length());
-                String tempval = val + str;
-                generateParentheses(n, open, newclose, tempval, set, answer);
-
-            }
-
+        for(int i = 0; i < sb.length(); i++){
+            solve(sb.insert(i,"()"),c+1,n);
+            sb.delete(i,i+2);
         }
     }
 }
