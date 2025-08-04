4365b5bf1bfe3ed6600f9f3f472fd7100d9dcac1
balaji379 <131160089+balaji379@users.noreply.github.com>
2025-08-04 15:24:00 +0530

    Time: 0 ms (100%), Space: 40.8 MB (18.06%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,19 +1,10 @@
 class Solution {
     public int climbStairs(int n) {
-           int step =0,itr = 2,first =1,second=1,sum=0;
-           if(n==1) return n;
-           else{
-            while(itr<=n){
-                  sum=first+second;
-                  if(itr==n)
-                      return sum;
-                  else {
-                    first=second;
-                    second=sum;
-                  }
-                  itr++;
-            }
-           }
-           return 0;
+        if(n<=1) return n;
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++) dp[i] = dp[i-1] + dp[i-2];
+        return dp[n];
     }
 }
