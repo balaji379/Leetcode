89bfdf6494ccbe0161ae29e62c7360a453a8d5d6
balaji379 <131160089+balaji379@users.noreply.github.com>
2025-08-15 23:30:43 +0530

    Time: 2121 ms (5.01%), Space: 64.7 MB (25.57%) - LeetHub

--- 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.java
+++ 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.java
@@ -1,22 +1,13 @@
 class Solution {
-    public int[] getAverages(int[] nums, int k) {
-           
-           long[] prefix = new long[nums.length + 1];
-           for (int i = 1; i < prefix.length; i++){
-             prefix[i]  = nums[i-1] + prefix[i - 1];
-           }
-        //    [0, 7, 11, 14, 23, 24, 32, 37, 39, 45]
-           int[] ans = new int[nums.length];
-           Arrays.fill(ans,-1);
-           for (int i = k; i < nums.length - k; i++){
-               int left = i - k;
-               int right = i + k;
-               if (left < 0 || right >= prefix.length)
-                    ans[i] = -1;
-                else {
-                       ans[i] = (int)((prefix[right + 1] - prefix[left]) / ((right - left) + 1));
-                }
-           }
-            return ans;
-    } 
+    public int[] getAverages(int[] nums, int k) { 
+        int n = nums.length;
+        int[] ans = new int[n];
+        Arrays.fill(ans,-1);
+        for(int i = k; i < n-k; i++){
+            long sum = 0;
+            for(int j = i-k; j <= i+k; j++) sum += nums[j];
+            ans[i] = (int)(sum/(k*2+1));
+        }
+        return ans;
+    }
 }
